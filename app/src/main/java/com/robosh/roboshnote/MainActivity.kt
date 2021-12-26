aae3d3cdf13ee763b8f5d48a8880bf1603cf6e6a
Orest Shemeliuk <orestshemeliuk@gmail.com>
2021-12-26 23:23:55 +0200

    add detekt to the project

--- app/src/main/java/com/robosh/roboshnote/MainActivity.kt
+++ app/src/main/java/com/robosh/roboshnote/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.robosh.roboshnote
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TIME_OUT = 2000L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
